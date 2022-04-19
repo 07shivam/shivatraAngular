b83fb2bab81e11aa3449d4ce5a3094fac0367c5a
shivam <shivam.bhargava7@gmail.com>
2022-04-19 17:34:03 +0530

    added home page

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'shivatra';
+  logoImg:string="assets/Images/1.PNG";
+  backgroundImg:string="assets/Images/2.jpg";
+
+}
